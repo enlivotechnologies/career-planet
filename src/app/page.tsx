c4077b790566d32b3fa7350706f426f54a51acb8
nishal77 <nishalpoojary09@gmail.com>
2026-02-16 15:09:40 +0530

    Fix build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <main>
       <Hero />
-      <TrustedCompanies />
+      {/* <TrustedCompanies /> */}
       <WhoWeAre />
       <Vision />
       {/* New Sections */}
